557a3affe65638d8dc9e8624f32ecb7fbb1f472c
Gal amrani <galamrani2@gmail.com>
2024-05-10 16:20:50 +0300

    Remove non-functional client-photo-editor code from main branch

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -13,32 +13,5 @@ import { environment } from 'src/environments/environment';
 })
 export class PhotoEditorComponent {
   @Input() member: Member | undefined;
-  newPhotoFile: File | undefined;
-  user: User | undefined;
-  baseUrl = environment.apiUrl;
 
-  constructor(private accountService: AccountService, private http: HttpClient) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => {
-        if (user) this.user = user
-      }
-    })
-  }
-
-  onFileSelected(event: any) {
-    this.newPhotoFile = event.target.files[0];
-  }
-
-  uploadPhoto() {
-    if (!this.newPhotoFile || !this.user) return;
-
-    const formData = new FormData();
-    formData.append('file', this.newPhotoFile);
-
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.user?.token
-    });
-
-    return this.http.post<any>(this.baseUrl + 'users/add-photo', formData, { headers: headers })
-  }
 }
